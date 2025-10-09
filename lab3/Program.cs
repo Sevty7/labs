2c83c70bc25e0885333a79b508d2c29b1868613e
Maksim Matsiuk <maximmatuk22000077@gmail.com>
2025-10-09 15:28:11 +0300

    class checking

--- lab3/Program.cs
+++ lab3/Program.cs
@@ -3,6 +3,16 @@
     {
         public static void Main(string[] args)
         {
+            Text text = new Text();
+            Sentence sentence = new Sentence();
+
+            sentence.tokens.Add(new Word("Hello"));
+            sentence.tokens.Add(new Punctuation(','));
+            sentence.tokens.Add(new Word(" World"));
+            sentence.tokens.Add(new Punctuation('!'));
+
+            text.AddSentence(sentence);
+            Console.WriteLine(text.ToString());
         }
     }
 }
--- lab3/Sentence.cs
+++ lab3/Sentence.cs
@@ -1,9 +1,26 @@
-﻿namespace lab3
+﻿using System.Text;
+
+namespace lab3
 {
     class Sentence
     {
-        public List<Token> Tokens { get; private set; } = new List<Token>();
+        public List<Token> tokens { get; private set; } = new List<Token>();
 
+        public override string ToString()
+        {
+            var result = new StringBuilder();
 
+            for (int i = 0; i < tokens.Count; i++)
+            {
+                var token = tokens[i];
+                result.Append(token.ToString());
+
+                if (token is Word && i < tokens.Count - 1 && tokens[i + 1] is Word)
+                {
+                    result.Append(' ');
+                }
+            }
+            return result.ToString();
+        }
     }
 }
